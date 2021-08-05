9db9dcff55247c106fd148eda62d92c983685d0f
Biswanath Tewari <biswanath@geekyants.com>
2021-08-05 16:50:55 +0530

    feat - integrated fontAwsome

--- screens/ViewImageScreen.js
+++ screens/ViewImageScreen.js
@@ -2,12 +2,24 @@ import React from 'react';
 import {View, StyleSheet, Image, Dimensions} from 'react-native';
 
 import color from "../config/color"
+import FontAwesome, {
+    SolidIcons,
+    RegularIcons,
+    BrandIcons,
+    parseIconFromClassName,
+  } from 'react-native-fontawesome';
 
 const ViewImageScreen = () => {
+
+    const parsedIcon = parseIconFromClassName('fas fa-times');
     return (
         <View style={styles.container} >
-            <View style={styles.closeButton} ></View>
-            <View style={styles.deleteButton} ></View>
+            <View style={styles.closeButton} >
+                <FontAwesome style={{color: color.primary,fontSize:40}} icon={SolidIcons.timesCircle} />
+            </View>
+            <View style={styles.deleteButton} >
+                <FontAwesome style={{color: color.secondary,fontSize:40}} icon={SolidIcons.trashAlt} />
+            </View>
             <Image style={styles.image} resizeMode="contain" source={require('../assets/chair.jpg')} />
         </View>
     );
@@ -15,9 +27,6 @@ const ViewImageScreen = () => {
 
 const styles = StyleSheet.create({
     closeButton: {
-        width: 50,
-        height: 50,
-        backgroundColor: color.primary,
         position: "absolute",
         top: 40,
         left: 30
@@ -27,9 +36,6 @@ const styles = StyleSheet.create({
         backgroundColor: color.black
     },
     deleteButton: {
-        width: 50,
-        height: 50,
-        backgroundColor: color.secondary,
         position: "absolute",
         top: 40,
         right: 30
