9bd675c65a480aa6e0a01fc348ff3876afcc1448
Biswanath Tewari <biswanath@geekyants.com>
2021-08-13 14:36:10 +0530

    feat - yup validation in the login screen

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import {StyleSheet, Image} from 'react-native';
 import {Formik} from 'formik';
+import * as yup from 'yup';
 
 import Screen from '../components/Screen';
 import AppTextInput from '../components/AppTextInput';
 import AppButton from '../components/AppButton';
+import AppText from '../components/AppText';
+
+const validationSchema = yup.object({
+  email: yup.string().required().email().label('Email'),
+  password: yup.string().required().min(4).label('Password'),
+});
 
 const LoginScreen = () => {
   return (
@@ -12,8 +19,9 @@ const LoginScreen = () => {
       <Image style={styles.logo} source={require('../assets/logo-red.png')} />
       <Formik
         initialValues={{email: '', password: ''}}
-        onSubmit={value => console.log(value)}>
-        {({handleChange, handleSubmit}) => (
+        onSubmit={value => console.log(value)}
+        validationSchema={validationSchema}>
+        {({handleChange, handleSubmit, errors}) => (
           <>
             <AppTextInput
               autoCapitalize="none"
@@ -24,6 +32,7 @@ const LoginScreen = () => {
               placeholder="Email"
               textContentType="emailAddress"
             />
+            <AppText customStyle={{color: 'red'}}>{errors.email}</AppText>
             <AppTextInput
               autoCapitalize="none"
               autoCorrect={false}
@@ -33,6 +42,7 @@ const LoginScreen = () => {
               secureTextEntry
               textContentType="password"
             />
+            <AppText customStyle={{color: 'red'}}>{errors.password}</AppText>
             <AppButton title="Login" onPress={handleSubmit} />
           </>
         )}
