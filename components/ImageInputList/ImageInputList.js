0e1a3b36f6e4554821b87779f1e7cc4363fab566
Biswanath Tewari <biswanath@geekyants.com>
2021-08-19 17:17:08 +0530

    fix - scrollToEnd() in ImageInputList

--- components/ImageInputList/ImageInputList.js
+++ components/ImageInputList/ImageInputList.js
@@ -1,19 +1,30 @@
-import React from 'react';
-import {View} from 'react-native';
+import React, {useRef} from 'react';
+import {ScrollView, View} from 'react-native';
 
 import ImageInput from '../ImageInput/ImageInput';
 import styles from './styles';
 
 const ImageInputList = ({imageUris = [], addImage, removeImage}) => {
+  const scrollView = useRef();
+
   return (
-    <View style={styles.container}>
-      {imageUris &&
-        imageUris.map(image => (
-          <View style={styles.image} key={image}>
-            <ImageInput imageUri={image} onChangeImage={removeImage} />
-          </View>
-        ))}
-      <ImageInput onChangeImage={addImage} />
+    <View>
+      <ScrollView
+        ref={scrollView}
+        horizontal
+        onContentSizeChange={() =>
+          scrollView.current.scrollToEnd({animated: true})
+        }>
+        <View style={styles.container}>
+          {imageUris &&
+            imageUris.map(image => (
+              <View style={styles.image} key={image}>
+                <ImageInput imageUri={image} onChangeImage={removeImage} />
+              </View>
+            ))}
+          <ImageInput onChangeImage={addImage} />
+        </View>
+      </ScrollView>
     </View>
   );
 };
