8ad65d13adb42dd3ee956fc6468c2879a04a2396
Biswanath Tewari <biswanath@geekyants.com>
2021-08-19 13:41:58 +0530

    feat - integrated images

--- App.js
+++ App.js
@@ -1,9 +1,62 @@
 import React, {useState} from 'react';
-import {View, StyleSheet} from 'react-native';
+import {View, StyleSheet, Button, Image} from 'react-native';
+import {launchCamera, launchImageLibrary} from 'react-native-image-picker';
+
+import Screen from './components/Screen';
 import ListingEditScreen from './screens/ListingEditScreen';
 
 const App = () => {
-  return <ListingEditScreen />;
+  const [imageUri, setImageUri] = useState(null);
+
+  const chooseImage = () => {
+    const options = {
+      title: 'Select Image',
+      customButtons: [
+        {name: 'customOptionKey', title: 'Choose Photo from Custom Option'},
+      ],
+      storageOptions: {
+        skipBackup: true,
+        path: 'images',
+      },
+    };
+
+    //* LAUNCHING THE IMAGE LIBRARY
+    launchImageLibrary(options, response => {
+      console.log('Response = ', response);
+      if (response.didCancel) {
+        alert('The image selection was cancelled');
+      } else if (response.error) {
+        console.log('ImagePicker Error: ', response.error);
+      } else if (response.customButton) {
+        console.log('User tapped custom button: ', response.customButton);
+        alert(response.customButton);
+      } else {
+        setImageUri(response.assets[0].uri); //* grabbing the image from the gallery and storing the location
+        console.log('response', JSON.stringify(response));
+      }
+    });
+  };
+
+  const rednerImage = () => {
+    if (imageUri)
+      return (
+        <Image source={{uri: imageUri}} style={{height: 100, width: 100}} />
+      );
+    else
+      return (
+        <Image
+          source={require('./assets/jacket.jpg')}
+          style={{height: 100, width: 100}}
+        />
+      );
+  };
+
+  return (
+    <Screen>
+      <Button title="Choose an image" onPress={() => chooseImage()} />
+      {rednerImage()}
+    </Screen>
+  );
 };
 
 export default App;
