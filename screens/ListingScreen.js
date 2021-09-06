4bc18ee59daebd479d7d813e3c45f545d52af073
Biswanath Tewari <biswanath@geekyants.com>
2021-09-06 17:28:18 +0530

    feat - error handling for fetching the apis

--- screens/ListingScreen.js
+++ screens/ListingScreen.js
@@ -5,25 +5,37 @@ import colors from '../config/color';
 
 import Card from '../components/Card';
 import Screen from '../components/Screen';
+import AppText from '../components/AppText';
+import AppButton from '../components/AppButton';
+
 import routes from '../navigations/routes';
 
 import listingApi from '../api/listing';
 
 const ListingScreen = ({navigation}) => {
   const [listings, setListings] = useState([]);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     loadListings();
-    console.log('listings' + listings);
   }, []);
 
   const loadListings = async () => {
     const res = await listingApi.getListings();
+    if (!res.ok) return setError(true);
+
+    setError(false);
     setListings(res.data);
   };
 
   return (
     <Screen style={styles.screen}>
+      {error && (
+        <>
+          <AppText>Something went wrong could not load the listings..</AppText>
+          <AppButton title="reload" onPress={loadListings} />
+        </>
+      )}
       <FlatList
         data={listings}
         keyExtractor={item => item.id.toString()}
