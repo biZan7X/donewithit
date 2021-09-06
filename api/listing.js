77f4d9df9cbca65fee003d595f7ded72894e4acd
Biswanath Tewari <biswanath@geekyants.com>
2021-09-06 20:37:48 +0530

    feat - post call

--- api/listing.js
+++ api/listing.js
@@ -4,6 +4,28 @@ const endpoint = '/listings';
 
 const getListings = () => apiClient.get(endpoint);
 
+const addListings = listing => {
+  const data = new FormData(); //^ this changes the content type of the data to multipart/form-data
+  data.append('title', listing.title);
+  data.append('price', listing.price);
+  data.append('categoryId', listing.category.value);
+  data.append('description', listing.description);
+
+  listing.images.forEach((image, index) => {
+    data.append('images', {
+      name: 'image' + index,
+      type: 'image/jpeg',
+      uri: image,
+    });
+  });
+
+  if (listing.location)
+    data.append('location', JSON.stringify(listing.location));
+
+  return apiClient.post(endpoint, data);
+};
+
 export default {
   getListings,
+  addListings,
 };
