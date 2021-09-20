277a57b121ff5c091b7bc57a1074d98895169779
Biswanath Tewari <biswanath@geekyants.com>
2021-09-20 13:19:21 +0530

    feat - signUp

--- api/auth.js
+++ api/auth.js
@@ -1,7 +1,10 @@
 import client from './client';
 
 const login = (email, password) => client.post('/auth', {email, password});
+const signup = (name, email, password) =>
+  client.post('/users', {name, email, password});
 
 export default {
   login,
+  signup,
 };
--- screens/RegisterScreen.js
+++ screens/RegisterScreen.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {StyleSheet} from 'react-native';
 import * as Yup from 'yup';
 
@@ -6,6 +6,10 @@ import Screen from '../components/Screen';
 import AppFormField from '../components/AppFormField';
 import SubmitButton from '../components/SubmitButton';
 import AppForm from '../components/AppForm';
+import ErrorMessage from '../components/ErrorMessage';
+
+import authApi from '../api/auth';
+import {useAuth} from '../auth/useAuth';
 
 const validationSchema = Yup.object().shape({
   name: Yup.string().required().label('Name'),
@@ -14,12 +18,37 @@ const validationSchema = Yup.object().shape({
 });
 
 function RegisterScreen() {
+  const [signupError, setSignupError] = useState();
+  const {logIn} = useAuth();
+
+  const handleSubmit = async ({name, email, password}) => {
+    const response = await authApi.signup(name, email, password);
+    if (!response.ok) {
+      if (response.data) setSignupError(response.data.error);
+      else {
+        setSignupError('An unexpected error occurred.');
+        console.log(response);
+      }
+      return;
+    }
+
+    setSignupError(null);
+
+    const {data} = await authApi.login(email, password);
+    logIn(data);
+  };
   return (
     <Screen style={styles.container}>
       <AppForm
         initialValues={{name: '', email: '', password: ''}}
-        onSubmit={values => console.log(values)}
+        onSubmit={handleSubmit}
         validationSchema={validationSchema}>
+        {signupError && (
+          <ErrorMessage
+            error="A user already exists with the given email."
+            visible={true}
+          />
+        )}
         <AppFormField
           autoCorrect={false}
           icon="account"
